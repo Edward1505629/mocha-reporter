073f9fdb6a9c861f2e5e63e404058480c96d60f8
jhoffner <jake.hoffner@gmail.com>
2015-02-18 15:58:48 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = Reporter;
+
+function Reporter(runner) {
+    var passes = 0;
+    var failures = 0;
+
+    runner.on('pass', function(test){
+        passes++;
+        console.log('pass: %s', test.fullTitle());
+    });
+
+    runner.on('fail', function(test, err){
+        failures++;
+        console.log('fail: %s -- error: %s', test.fullTitle(), err.message);
+    });
+
+    runner.on('end', function(){
+        console.log('yEAAAH');
+        console.log('end: %d/%d', passes, passes + failures);
+        process.exit(failures);
+    });
+}
